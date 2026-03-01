65b07b383d1ceca8d6c487d97d9e21e78f37aaab
HongweiYang <761807812@qq.com>
2026-03-01 20:18:51 +0800

    fix(autoselect): correct parameter name mismatch in evaluator wrappers
    - NormalizationEvaluator: base_layer -> source_layer
    - ImputationEvaluator: base_layer -> source_layer
    
    - All tests passing

--- scptensor/autoselect/evaluators/imputation.py
+++ scptensor/autoselect/evaluators/imputation.py
@@ -149,7 +149,7 @@ class ImputationEvaluator(BaseEvaluator):
             return func(
                 container=container,
                 assay_name=assay_name,
-                base_layer=source_layer,
+                source_layer=source_layer,
                 new_layer_name=f"{source_layer}_{func.__name__}",
                 **kwargs,
             )
